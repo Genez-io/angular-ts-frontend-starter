e2c030230ac1247d4cc4f008ce4ef1bb1cf83b81
Virgil993 <turcu.virgil@gmail.com>
2024-02-02 17:18:18 +0200

    angular template done, still need to do eslint

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BackendService } from '@genezio-sdk/(•◡•)project-name(•◡•)_(•◡•)region(•◡•)';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  name = '';
+  response = '';
+
+  async sayHello() {
+    this.response = await BackendService.hello(this.name);
+  }
+}
