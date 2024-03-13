aa15e80186a821c9e18a7650b106b6c4bf9129cf
Costin Sin <sin.costinrobert@gmail.com>
2024-03-13 17:04:03 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { BackendService } from '@genezio-sdk/(•◡•)project-name(•◡•)_(•◡•)region(•◡•)';
+import { BackendService } from '@genezio-sdk/(•◡•)project-name(•◡•)';
 
 @Component({
   selector: 'app-root',
